4f04043e0cb4336173c1ff7cab818eb985b10f30
Ali Erhan Aydemir <alierhan.aydemir@deu.edu.tr>
2017-08-29 12:45:14 +0300

    initials...

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const merge = require('webpack-merge');
+
+const TARGET = process.env.npm_lifecycle_event;
+const common = {
+    app: path.join(__dirname, 'app'),
+    build: path.join(__dirname, 'build')
+};
+
+module.exports = {
+// Entry accepts a path or an object of entries. We'll be using the
+// latter form given it's convenient with more complex configurations.
+    entry: {
+        app: PATHS.app
+    },
+    output: {
+        path: PATHS.build,
+        filename: 'bundle.js'
+    }
+};
+
+// Default configuration
+if (TARGET === 'start' || !TARGET) {
+    module.exports = merge(common, {});
+}
+if (TARGET === 'build') {
+    module.exports = merge(common, {});
+}
